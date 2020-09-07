b8a5bd7ae29c3528c5a3c7a5b6c0b22420f6e7f1
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-07 14:43:24 +0300

    added get data from api and vuex store

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -3,10 +3,9 @@
     <h1>Catalog</h1>
     <div class="v-catalog">
       <vCatalogItem
-        v-for="product in products"
+        v-for="product in PRODUCTS"
         :key="product.article"
         :product_data="product"
-        @sendArticleToParent="showToConsole"
       />
     </div>
   </div>
@@ -14,70 +13,30 @@
 
 <script>
 import vCatalogItem from "./v-catalog-item";
+import {mapActions, mapGetters} from 'vuex'
 export default {
   name: "v-catalog",
   components: {
     vCatalogItem,
   },
   methods: {
-      showToConsole(data) {
-          console.log(data)
-      }
+      ...mapActions([
+        'GET_PRODUCTS_FROM_API'
+      ])
+  },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API()
   },
   data() {
     return {
-      products: [
-        {
-          image: "1.jpg",
-          name: "Call of Duty",
-          price: 2100,
-          article: "T1",
-          available: true,
-          category: "Action",
-        },
-        {
-          image: "2.jpg",
-          name: "Divinity",
-          price: 3150,
-          article: "T2",
-          available: true,
-          category: "RPG",
-        },
-        {
-          image: "3.jpg",
-          name: "Prey",
-          price: 4200,
-          article: "T3",
-          available: false,
-          category: "Action",
-        },
-        {
-          image: "4.jpg",
-          name: "Dishonored",
-          price: 5300,
-          article: "T4",
-          available: true,
-          category: "Action",
-        },
-        {
-          image: "5.jpg",
-          name: "Flight Simulator",
-          price: 6500,
-          article: "T5",
-          available: false,
-          category: "Simulater",
-        },
-        {
-          image: "6.jpg",
-          name: "World of Warcraft",
-          price: 8700,
-          article: "T6",
-          available: true,
-          category: "Online RPG",
-        },
-      ],
+      
     };
   },
+  computed: {
+    ...mapGetters([
+        'PRODUCTS'
+      ])
+  }
 };
 </script>
 
