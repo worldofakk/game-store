b60b61c0db962bca25b00397aa280da4f5085816
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-10 11:50:12 +0300

    added range slider

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -5,13 +5,36 @@
       Cart: {{CART.length}}
     </div>
     </router-link>
-    
     <h1>Catalog</h1>
-    <vSelect
-      :selected="selected"
-      :options="categories"
-      @select="sortByCategories"
-    />
+    <div class="filters">
+      <vSelect
+        :selected="selected"
+        :options="categories"
+        @select="sortByCategories"
+      />
+      <div class="range-slider">
+        <input 
+          type="range" 
+          min="0" 
+          max="9000" 
+          step="10"
+          v-model.number="minPrice"
+          @change="setRangeSldiers"
+        >
+        <input 
+          type="range" 
+          min="0" 
+          max="9000" 
+          step="10" 
+          v-model.number="maxPrice"
+          @change="setRangeSldiers"
+        >
+        <div class="range-values">
+          <p class="min">{{minPrice}}</p>
+          <p class="max">{{maxPrice}}</p>
+        </div>
+      </div>
+    </div>
     <div class="v-catalog__list">
       <vCatalogItem
         v-for="product in filteredProducts"
@@ -45,18 +68,30 @@ export default {
         console.log('kek')
       },
       sortByCategories(category) {
-        this.sortedProducts = []
         let self = this
-        this.PRODUCTS.map(function(item) {
-          if (item.category === category.name) {
-            self.sortedProducts.push(item);
-          }
+        this.sortedProducts = [...this.PRODUCTS]
+        this.sortedProducts = this.sortedProducts.filter(function(item) {
+          return item.price >=self.minPrice && item.price <= self.maxPrice
         })
-        this.selected = category.name
+        if (category) {
+          this.sortedProducts = this.sortedProducts.filter(function(e) {
+          self.selected = category.name
+          return e.category === category.name
+        })
+        }
+      },
+      setRangeSldiers() {
+        if (this.minPrice > this.maxPrice) {
+          let temp = this.maxPrice
+          this.maxPrice = this.minPrice
+          this.minPrice = temp
+        }
+        this.sortByCategories()
       }
   },
   mounted() {
-    this.GET_PRODUCTS_FROM_API()
+    this.GET_PRODUCTS_FROM_API(),
+    this.sortByCategories()
   },
   data() {
     return {
@@ -68,9 +103,9 @@ export default {
         {name: 'Online RPG', value: 'Online RPG'}
       ],
       selected: 'All',
-      sortedProducts: [
-
-      ]
+      sortedProducts: [],
+      minPrice: 0,
+      maxPrice: 9000
     };
   },
   computed: {
@@ -107,6 +142,45 @@ export default {
       right: 0;
       box-shadow: 0 0 8px 0 #e3e3e3;
       padding: 15px;
+    }
+  }
+  .filters {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .range-slider {
+    width: 200px;
+    margin-top: 10px;
+    text-align: center;
+    position: relative;
+  }
+  .range-slider svg, .range-slider input[type=range] {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    z-index: 2;
+    position: relative;
+    top: 2px;
+    margin-top: -7px;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    background: #419ae9;
+    border-radius: 15px;
+  }
+  .range-values {
+    position: absolute;
+    .min {
+      position: absolute;
+      left: -37px;
+      top: -13px;
+    }
+    .max {
+      position: absolute;
+      left: 130px;
+      top: -13px;
     }
   }
 
